c095c6f511ca9c54dbd736e7976cd309bbd8ef29
builtbyfaiz <229525323+builtbyfaiz@users.noreply.github.com>
2026-07-29 23:28:49 +0500

    feat(controller) : added toggles for rotation and target

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -1,49 +1,62 @@
-#include <iostream>
 #include "controller.h"
+#include <iostream>
 
 // Check key presses, decide movement
-void Controller::handleInput() 
+void Controller::handleInput()
 {
     // Reset Deltas
     movementDelta = raylib::Vector3(0.0f, 0.0f, 0.0f);
     rotationDelta = raylib::Vector3(0.0f, 0.0f, 0.0f);
-    
-    float pitch = GetMouseDelta().x   * mouseSensitivity;
-    float yaw   = GetMouseDelta().y   * mouseSensitivity;
-    float roll  = GetMouseWheelMove() * mouseSensitivity;
-    
-    if(IsKeyPressed(KEY_O)) controlMode = ControlMode::Cam;   // Camera
-    if(IsKeyPressed(KEY_P)) controlMode = ControlMode::Shape; // Shape #TODO make selector for shapes
-    
+
+    pitch = GetMouseDelta().x   * mouseSensitivity;
+    yaw   = GetMouseDelta().y   * mouseSensitivity;
+    roll  = GetMouseWheelMove() * mouseSensitivity * 5.0; // Provide boost to the wheels movement
+
+    if (IsKeyDown(KEY_R)) toggleRotationMode();
+    if (IsKeyPressed(KEY_T)) toggleControlMode();
+    if (IsKeyDown(KEY_ESCAPE)) EnableCursor();
+     
     // Decide change in x,y,z based on keyboard input i.e update the move Vector
-    if (IsKeyDown(KEY_W)) movementDelta.SetZ( moveSpeed); // Forward
-    if (IsKeyDown(KEY_S)) movementDelta.SetZ(-moveSpeed); // Backward
-    if (IsKeyDown(KEY_A)) movementDelta.SetX(-moveSpeed); // Left
-    if (IsKeyDown(KEY_D)) movementDelta.SetX( moveSpeed); // Right
-    if (IsKeyDown(KEY_Q)) movementDelta.SetY( moveSpeed); // UP
-    if (IsKeyDown(KEY_E)) movementDelta.SetY(-moveSpeed); // Down
-
-    if(IsKeyDown(KEY_R)) rotationDelta = Vector3{-yaw, -pitch, roll}; // Rotate by Mouse when key activated
-
-    if(IsKeyDown(KEY_ESCAPE)) EnableCursor();
+    if (IsKeyDown(KEY_W)) movementDelta.SetZ( moveSpeed);  // Forward
+    if (IsKeyDown(KEY_S)) movementDelta.SetZ(-moveSpeed);  // Backward
+    if (IsKeyDown(KEY_A)) movementDelta.SetX(-moveSpeed);  // Left
+    if (IsKeyDown(KEY_D)) movementDelta.SetX( moveSpeed);  // Right
+    if (IsKeyDown(KEY_E)) movementDelta.SetY( moveSpeed);  // UP
+    if (IsKeyDown(KEY_Q)) movementDelta.SetY(-moveSpeed);  // Down
 }
 
 // Move stuff
 void Controller::update(World &world)
-{   
-    // Camera Mode
-    if (controlMode == ControlMode::Cam ) {
-        world.camera.move(movementDelta);
-        world.camera.rotate(rotationDelta); 
-    }
+{
+    if(rotationMode) 
+        rotationDelta = Vector3{-yaw, -pitch, roll}; 
     
+        // Camera Mode
+    if (controlMode == ControlMode::Cam)
+    {
+        world.camera.move(movementDelta);
+        world.camera.rotate(rotationDelta);
+    }
+
     //Individual shape mode
-    else if (controlMode == ControlMode::Shape) {
+    else if (controlMode == ControlMode::Shape)
+    {
         world.shapes[selectedShapeIndex].move(movementDelta);
         world.shapes[selectedShapeIndex].rotate(rotationDelta);
     }
-        
+
     else
         std::cout << "invalid Move Mode";
 }
 
+// Toggles between controlling shapes or camera
+void Controller::toggleControlMode()
+{
+    controlMode = (controlMode == ControlMode::Cam) ? ControlMode::Shape : ControlMode::Cam;
+}
+
+// Toggles rotations via mouse
+void Controller::toggleRotationMode() 
+{
+    rotationMode = rotationMode == false ? true : false;
+}
--- src/controller/controller.h
+++ src/controller/controller.h
@@ -2,26 +2,30 @@
 #include "model/world.h"
 #include "raylib-cpp.hpp" // IWYU pragma: keep
 
-
 // handles input, camera movement, shape movement
 class Controller
 {
   private:
     enum class ControlMode
     {
-        Cam,  // Move / Rotate Camera
-        Shape // Move / Rotate Shapes
+        Cam,
+        Shape
     } controlMode{ControlMode::Cam};
 
+    float moveSpeed          = 2;      // Control Speed of movement
+    float mouseSensitivity   = 0.01;   // Control Speed of rotations
+    int   selectedShapeIndex = 0;      // Index of shapes in the world array
+    bool  rotationMode       = false;  // Toggle rotation
+    
+    float pitch, yaw, roll; // Vars for rotatons
+
+    raylib::Vector3 movementDelta{0, 0, 0};
+    raylib::Vector3 rotationDelta{0, 0, 0};
+
+    void toggleControlMode();   // Decide if to move camera or shapes
+    void toggleRotationMode();  // Decide if to move camera or shapes
+
   public:
-
-    float moveSpeed          = 2;  // Control Speed of movement
-    float mouseSensitivity   = 0.01; // Control Speed of rotations 
-    int   selectedShapeIndex = 0;    // Index of shapes in the world array
-
-    raylib::Vector3 movementDelta{0,0,0}; 
-    raylib::Vector3 rotationDelta{0,0,0}; 
-
-    void handleInput();         // Decide if an action is needed
-    void update(World &world);  // Perform the needed action
+    void handleInput();        // Decide if an action is needed
+    void update(World &world); // Perform the needed action
 };

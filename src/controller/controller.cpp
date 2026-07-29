e60fa667556d29e53eedb0ec8d6c099032226111
builtbyfaiz <229525323+builtbyfaiz@users.noreply.github.com>
2026-07-29 18:31:02 +0500

    refactor(controller) : organized individual mouse and keyboard code

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -4,38 +4,42 @@
 // Check key presses, decide movement
 void Controller::handleInput() 
 {
-    // By default Do no movement, reset delta
-    // Vector2 mouseDelta = GetMouseDelta();
-
-    // float yaw   += mouseDelta.x * controlSpeed;   
-    // float pitch += mouseDelta.y * controlSpeed;
+    // Reset Deltas
+    movementDelta = raylib::Vector3(0.0f, 0.0f, 0.0f);
+    rotationDelta = raylib::Vector3(0.0f, 0.0f, 0.0f);
     
-    movementDelta = raylib::Vector3{0,0,0};
+    float pitch = GetMouseDelta().x   * mouseSensitivity;
+    float yaw   = GetMouseDelta().y   * mouseSensitivity;
+    float roll  = GetMouseWheelMove() * mouseSensitivity;
     
-    if(IsKeyPressed(KEY_O)) controlMode = ControlMode::Cam; // Camera
-    if(IsKeyPressed(KEY_P)) controlMode = ControlMode::Shape;; // Shape #TODO make selector for shapes
-
-    // Decide change in x,y,z based on input i.e construct the move Vector
-    if (IsKeyDown(KEY_A)) movementDelta.x -= controlSpeed;
-    if (IsKeyDown(KEY_D)) movementDelta.x += controlSpeed;
+    if(IsKeyPressed(KEY_O)) controlMode = ControlMode::Cam;   // Camera
+    if(IsKeyPressed(KEY_P)) controlMode = ControlMode::Shape; // Shape #TODO make selector for shapes
     
-    if (IsKeyDown(KEY_W)) movementDelta.y += controlSpeed;
-    if (IsKeyDown(KEY_S)) movementDelta.y -= controlSpeed;
+    // Decide change in x,y,z based on keyboard input i.e update the move Vector
+    if (IsKeyDown(KEY_W)) movementDelta.SetY( moveSpeed); // UP
+    if (IsKeyDown(KEY_S)) movementDelta.SetY(-moveSpeed); // Down
+    if (IsKeyDown(KEY_A)) movementDelta.SetX(-moveSpeed); // Left
+    if (IsKeyDown(KEY_D)) movementDelta.SetX( moveSpeed); // Right
+    if (IsKeyDown(KEY_I)) movementDelta.SetZ( moveSpeed); // Forward
+    if (IsKeyDown(KEY_K)) movementDelta.SetZ(-moveSpeed); // Backward
 
-    if (IsKeyDown(KEY_I)) movementDelta.z += controlSpeed;
-    if (IsKeyDown(KEY_K)) movementDelta.z -= controlSpeed;
+    if(IsKeyDown(KEY_R)) rotationDelta = Vector3{-yaw, -pitch, roll}; // Rotate by Mouse when key activated
 }
 
 // Move stuff
 void Controller::update(World &world)
 {   
     // Camera Mode
-    if (controlMode == ControlMode::Cam )
+    if (controlMode == ControlMode::Cam ) {
         world.camera.move(movementDelta);
-    
+        world.camera.rotate(rotationDelta); 
+    }
+
     //Individual shape mode
-    else if (controlMode == ControlMode::Shape)
+    else if (controlMode == ControlMode::Shape) {
         world.shapes[selectedShapeIndex].move(movementDelta);
+        world.shapes[selectedShapeIndex].rotate(rotationDelta);
+    }
         
     else
         std::cout << "invalid Move Mode";
